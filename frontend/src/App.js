a625889a52ea22e2a8c6f6cdf2acf8c31ff61f47
priyanshu-baran <baranpriyanshu3@gmail.com>
2024-08-13 11:43:14 +0530

    Added environment variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { Amplify } from 'aws-amplify';
 
 import { AdminDashboard } from './AdminDashboard';
 import { Home } from './Home';
 import { Preloader } from './Preloader';
 import { Profile } from './Profile';
+import awsConfig from './aws-exports';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: awsConfig.aws_user_pools_id,
+      userPoolClientId: awsConfig.aws_user_pools_web_client_id,
+    },
+  },
+});
+
+// Amplify.configure({
+//   Auth: {
+//     Cognito: {
+//       userPoolId: process.env.REACT_APP_AMPLIFY_USERPOOL_ID,
+//       userPoolClientId: process.env.REACT_APP_AMPLIFY_WEBCLIENT_ID,
+//     },
+//   },
+// });
 
 export const App = () => {
   const [isLoading, setIsLoading] = useState(true);
